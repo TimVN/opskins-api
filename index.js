7b61e3ef57e4c5de6bdb1e40702aef545be5b515
timvn <tim.van.norde1993@gmail.com>
2017-11-20 01:34:41 +0100

    Added test method

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const request = require('request-promise');
 class OPSkins {
     constructor(apiKey) {
         this.apiKey = apiKey;
+        this.opRequest = new (require('./modules/request'))(apiKey);
         this.User = require('./modules/user')(apiKey);
         this.Pricing = require('./modules/pricing')(apiKey);
         this.Sales = require('./modules/sales')(apiKey);
@@ -10,10 +11,9 @@ class OPSkins {
         this.Status = require('./modules/status')(apiKey);
         this.Support = require('./modules/support')(apiKey);
         this.Inventory = require('./modules/inventory')(apiKey);
-    }
-
-    opRequest(uri) {
-        return request('https://api.opskins.com/' + path + '?key=' + this.apiKey);
+        this.Test = () => {
+            return this.opRequest.make('ITest/TestAuthed/v1');
+        };
     }
 }
 
