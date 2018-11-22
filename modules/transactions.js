1e94ec0dbbfbff9ca81de3045c363747311ee145
timvn <tim.van.norde1993@gmail.com>
2018-11-22 15:46:41 +0100

    Changed transferfunds method to use POST

--- modules/transactions.js
+++ modules/transactions.js
@@ -3,7 +3,7 @@ module.exports = (apiKey) => {
 
     return {
         TransferFunds: (amount, steamID = '', uID = '') => {
-            return opRequest.make('https://api.opskins.com/ITransactions/TransferFunds/v1/', {id64: steamID, uid: uID, amount: amount});
+            return opRequest.make('https://api.opskins.com/ITransactions/TransferFunds/v1/', {id64: steamID, uid: uID, amount: amount}, 'post');
         },
     }
 };
